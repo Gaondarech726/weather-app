4b0a1fbb00394d68ec78536fce316115f9469654
Dremeret <terletskyyd2010@gmail.com>
2024-12-20 20:45:41 +0200

    Footer layout

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,9 +1,26 @@
+import Facebook from './facebook.svg';
 import './footer.scss';
+import Instagram from './instagram.svg';
+import LogotypeFooter from './LogotypeFooter.svg';
+import Whatsapp from './whatsapp.svg';
 const Footer = () => {
   return (
-    <div >
-     Привіт, я Дмитро
+    <div className="footer-div-main">
+      <img src={LogotypeFooter} alt="" />
+      <div className="footer-div-text">
+        <h4>Address</h4>
+        <p>Svobody str. 35</p>
+        <p>Kyiv</p>
+        <p>Ukraine</p>
+      </div>
+      <div className="footer-contactUs">
+        <h3>Contact us</h3>
+        <img src={Instagram} alt="" />
+        <img src={Facebook} alt="" />
+        <img src={Whatsapp} alt="" />
+      </div>
     </div>
   );
 };
+
 export default Footer;
