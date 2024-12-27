0185d2756e015d656afa1e02ee4bc953cff7aaa0
Mkqupy <121451774+Mkqupy@users.noreply.github.com>
2024-12-27 21:42:00 +0200

    save swiper

--- src/components/Section-5/section-5.jsx
+++ src/components/Section-5/section-5.jsx
@@ -1,5 +1,98 @@
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+// import 'swiper/css/scrollbar';
+
 import './section-5.scss';
+
+import BerestenImg from './images/DimaBeresten.png';
+import HoncharenkoImg from './images/AntonHoncharenko.png';
+import StepenkoImg from './images/KhrystynaStepenko.png';
+import KalinichenkoImg from './images/MariaKalinichenko.jpg';
+// import BerestenImg from './images/DimaBeresten.png';
+// import BerestenImg from './images/DimaBeresten.png';
+// import BerestenImg from './images/DimaBeresten.png';
+// import BerestenImg from './images/DimaBeresten.png';
+
 const Section5 = () => {
-  return 'Hi, its Maria';
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={swiper => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <div className="slider">
+        <SwiperSlide>
+          <div className="slide">
+            <img className="BerestenImg" src={BerestenImg} alt="" />
+            <p>Project Manager</p>
+            <p>Dima Beresten</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide">
+            <img src={HoncharenkoImg} alt="" />
+            <p>Tech Lead, Frontend Developer</p>
+            <p>Anton Honcharenko</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide">
+            <img src={StepenkoImg} alt="" />
+            <p>Scrum Master, Frontend Developer</p>
+            <p>Khrystyna Stepenko</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide">
+            <img src={KalinichenkoImg} alt="" />
+            <p>Christmas DJ, Frontend Developer</p>
+            <p>Maria Kalinichenko</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide">
+            <img className="BerestenImg" src={BerestenImg} alt="" />
+            <p>Project Manager</p>
+            <p>Dima Beresten</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide">
+            <img src={HoncharenkoImg} alt="" />
+            <p>Tech Lead, Frontend Developer</p>
+            <p>Anton Honcharenko</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide">
+            <img src={StepenkoImg} alt="" />
+            <p>Scrum Master, Frontend Developer</p>
+            <p>Khrystyna Stepenko</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide">
+            <img src={KalinichenkoImg} alt="" />
+            <p>Christmas DJ, Frontend Developer</p>
+            <p>Maria Kalinichenko</p>
+          </div>
+        </SwiperSlide>
+      </div>
+      ...
+    </Swiper>
+  );
 };
 export default Section5;
